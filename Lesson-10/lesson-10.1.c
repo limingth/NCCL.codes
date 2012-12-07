32860c3966d2f4c615f5d1d2f730b495c39a0d89
Li Ming <limingth@gmail.com>
2012-12-07 10:11:09 +0800

    Update Lesson-10/lesson-10.1.c

--- Lesson-10/lesson-10.1.c
+++ Lesson-10/lesson-10.1.c
@@ -1,58 +1,55 @@
 #include <stdio.h>
 
-#define ALL_NUM    	100
-#define COUNT_NUM	3
-#define OUT_NUM		3
+#define ALL_NUM     100
+#define COUNT_NUM   3
+#define OUT_NUM     3
 
 /* people id array such as (1,2,3,4,5,6) */
 int people[ALL_NUM];
 
 int main(void)
 {
-	int left;	/* left people number */
-	int pos;	/* which pos */
-	int step;	/* which step */
-	int i;
+    int left;   /* left people number */
+    int pos;    /* which pos */
+    int step;   /* which step */
+    int i;
 
-	int counter = 0;
+    int counter = 0;
 
-	left = ALL_NUM;
-	pos = 0;
-	step = 0;
+    left = ALL_NUM;
+    pos = 0;
+    step = 0;
 
-	/* init id as 1,2,3,4,5,6 */
-	for (i = 0; i < ALL_NUM; i++)
-		people[i] = i + 1;
+    /* init id as 1,2,3,4,5,6 */
+    for (i = 0; i < ALL_NUM; i++)
+        people[i] = i + 1;
 
-	/* if there is still people in queue */
-	while (left > 0)
-	{
-		/* pos++?  step++?   left--? */
-		if (people[pos] > 0)
-			step++;
+    /* if there is still people in queue */
+    while (left > 0)
+    {
+        /* pos++?  step++?   left--? */
+        if (people[pos] > 0)
+            step++;
 
-		if (step == OUT_NUM && people[pos] != 0)
-		{
-			printf("%d out \n", people[pos]);
-			people[pos] = 0;
-			left--;
-		}
+        if (step == OUT_NUM && people[pos] != 0)
+        {
+            printf("%d out \n", people[pos]);
+            people[pos] = 0;
+            left--;
+        }
 
-#if 0
-		printf("counter=%d ", counter++);
-		printf("(pos=%d, step=%d, left=%d) : ", pos, step, left);		
-		getchar();
-#endif
+    #if 1
+        pos = ++pos % ALL_NUM;
+        step = step % COUNT_NUM;
+    #else   
+        pos++;
+        if (pos == ALL_NUM)
+            pos = 0;
 
-		//pos = ++pos % ALL_NUM;
-		pos++;
-		if (pos == ALL_NUM)
-			pos = 0;
+        if (step == COUNT_NUM)
+            step = 0;
+    #endif
+    }
 
-		//step = step % COUNT_NUM;
-		if (step == COUNT_NUM)
-			step = 0;
-	}
-
-	return 0;
+    return 0;
 }
